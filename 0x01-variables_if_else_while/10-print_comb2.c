cf4051903a3fcd4fb21be247569adef62b2c4376
paurbano <paurbano@gmail.com>
2019-09-19 23:46:57 +0000

    prints the numbers from 00 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,26 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+int i, j;
+
+for (j = 48; j < 58; j++)
+{
+for (i = 48 ; i < 58 ; i++)
+{
+putchar(j);
+putchar(i);
+if (j != 57 || i != 57)
+putchar(44);
+
+putchar(32);
+}
+}
+putchar('\n');
+return (0);
+}
