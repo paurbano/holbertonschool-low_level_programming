45a471147b68eaa72049172d1676bd21bcf0044a
paurbano <paurbano@gmail.com>
2019-09-19 19:47:54 +0000

    prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+ #include <stdio.h>
+int main(void)
+{
+char letra = 'a';
+char retorno = '\n';
+while (letra <= 'z')
+{
+putchar (letra);
+letra++;
+}
+putchar (retorno);
+return (0);
+}
