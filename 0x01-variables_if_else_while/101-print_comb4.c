3ecbf79cdd769da6efdf7a771b43955a11f6c135
paurbano <55122159+paurbano@users.noreply.github.com>
2019-09-19 20:40:44 -0500

    Made from mobile...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,31 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
 
+int main(void)
+{
+int i, j, k ;
+
+for (j = 48 ; j < 58 ; j++)
+{
+for (i = j+1 ; i < 58 ; i++)
+{
+for (k = i+2 ; k < 58 ; k++)
+{
+putchar(j);
+putchar(i);
+putchar(k);
+if (j != 57 || i != 57 || k != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
