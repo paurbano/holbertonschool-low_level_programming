72726ce5377ef3c5885bba5aedeca5786f8da63f
paurbano <paurbano@gmail.com>
2019-09-20 03:27:07 +0000

    prints all possible different combinations of three digits.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,12 +13,12 @@ for (j = 48 ; j < 58 ; j++)
 {
 for (i = j + 1 ; i < 58 ; i++)
 {
-for (k = i + 2 ; k < 58 ; k++)
+for (k = i + 1 ; k < 58 ; k++)
 {
 putchar(j);
 putchar(i);
 putchar(k);
-if (j != 57 || i != 57 || k != 57)
+if (j != 55 || i != 56 || k != 57)
 {
 putchar(44);
 putchar(32);
