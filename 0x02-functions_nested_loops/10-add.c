90cd45366b7dd9059a3af6e8660d5345349d4263
paurbano <paurbano@gmail.com>
2019-09-24 18:52:38 +0000

    adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+ * add - writes the character c to stdout
+ * @a: xyz
+ * @b: xyz
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
