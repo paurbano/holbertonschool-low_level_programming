d83a947e37270cd7df043b0559b0ccfa84ee0ab1
paurbano <paurbano@gmail.com>
2019-09-24 13:58:39 +0000

    prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,16 +1,18 @@
 #include "holberton.h"
-
+/**
+ * main - check the code for Holberton School students.
+ *
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-_putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
-_putchar('t');
-_putchar('o');
-_putchar('n');
+char *holberton = "Holberton";
+int i;
+for (i = 0; holberton[i] != '\0'; i++)
+{
+_putchar(holberton[i]);
+}
 _putchar('\n');
 return (0);
 }
