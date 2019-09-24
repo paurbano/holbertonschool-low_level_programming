08bf36c71fbb67169d2dcdecaddf83104b193e4b
paurbano <paurbano@gmail.com>
2019-09-24 19:46:48 +0000

    prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include"holberton.h"
+/**
+ * jack_bauer - writes the character c to stdout
+ * 
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void jack_bauer(void)
+{
+int i, j;
+
+for (i =0 ; i <=24  ; i++)
+{
+for (j = 0 ; j <60  ; j++)
+{
+_putchar(48+i/10);
+_putchar(48+i%10);
+_putchar(58);
+_putchar(48+j/10);
+_putchar(48+j%10);
+_putchar('\n');
+}
+}
+}
