cbe023878d798ad86fb173d75982ad2ccd2aa1df
paurbano <paurbano@gmail.com>
2019-09-24 14:47:16 +0000

    function that checks for alphabetic character.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,17 @@
 #include<ctype.h>
-
+/**
+ * _isalpha - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _isalpha(int c)
 {
- if(c>=97 && c<=122) || (c>=48 && c<=57) || (c>=65 && c<=90) 
-  return (1);
- else
-  return (0);
+int i;
+if ((c >= 97 && c <= 122) || (c >= 48 && c <= 57) || (c >= 65 && c <= 90))
+i = 1;
+else
+i = 0;
+return (i);
 }
