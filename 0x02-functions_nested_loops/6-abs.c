2984866f06a22fee0484d695d0b43e53a119c308
paurbano <paurbano@gmail.com>
2019-09-24 16:39:05 +0000

    absolute value of an integer.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+/**
+ * _abs - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _abs(int c)
+{
+  return (abs(c));
+}
