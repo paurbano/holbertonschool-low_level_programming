1c0f42bbe398dc52db2d63fb6ee8115273502172
paurbano <paurbano@gmail.com>
2019-09-24 18:07:20 +0000

    prints the last digit of a number.

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include"holberton.h"
+#include <stdlib.h>
+/**
+ * print_last_digit - writes the character c to stdout
+ * @n: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int print_last_digit(int n)
+{
+int last;
+last = abs(n % 10);
+_putchar('0'+last);
+return (last);
+}
