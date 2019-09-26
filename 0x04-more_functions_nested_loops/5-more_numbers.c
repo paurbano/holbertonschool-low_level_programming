8521ceb371d36563da7ed424af9e5a08cf0dfa33
paurbano <paurbano@gmail.com>
2019-09-26 18:11:22 +0000

    prints 10 times the numbers, from 0 to 14, followed by a new line.only use _putchar three times in your code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,13 +12,10 @@ for (i = 0; i <= 10 ; i++)
 {
 for (j = 0; j < 15 ; j++)
 {
-if (j < 10)
-_putchar ('0' + j);
-else
-{
+if (j > 9)
 _putchar ('0' + j / 10);
+
 _putchar ('0' + j % 10);
-}
 }
 _putchar (retorno);
 }
