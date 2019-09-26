474abcb66224f72d8625ca4d948b25259eda362c
paurbano <paurbano@gmail.com>
2019-09-26 15:05:02 +0000

    multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - writes the character c to stdout
+ * @a: first parameter
+ * @b: second parameter
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
