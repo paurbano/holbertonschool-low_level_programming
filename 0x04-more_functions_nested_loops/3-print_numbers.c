79495646f31c022057af73f8a86ddc4f6653fe9a
paurbano <paurbano@gmail.com>
2019-09-26 15:22:49 +0000

    prints the numbers, from 0 to 9, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_numbers - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+char retorno = '\n';
+int i;
+for (i = 48; i < 58 ; i++)
+{
+_putchar (i);
+}
+_putchar (retorno);
+}
