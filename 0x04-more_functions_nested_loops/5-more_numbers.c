18a5bf34e2fa7a9119f61e31b0d1d1ed99d8cc91
paurbano <paurbano@gmail.com>
2019-09-26 16:48:29 +0000

    prints 10 times the numbers, from 0 to 14, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * more_numbers - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+char retorno = '\n';
+int i, j;
+for (i = 0; i <= 10 ; i++)
+{
+for (j = 0; j < 15 ; j++)
+{
+if (j < 10)
+_putchar ('0' + j);
+else
+{
+_putchar ('0' + j / 10);
+_putchar ('0' + j % 10);
+}
+}
+_putchar (retorno);
+}
+}
