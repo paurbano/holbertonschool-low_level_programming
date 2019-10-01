ef97a482c52483d71b9b060ad6a023d8b980ac3c
paurbano <paurbano@gmail.com>
2019-10-01 05:26:46 +0000

    function that swaps the values of two integers.

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * swap_int - check the code for Holberton School students.
+ * @a: parameter
+ * @b: parameter
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+int c;
+c = *a;
+*a = *b;
+*b = c;
+}
