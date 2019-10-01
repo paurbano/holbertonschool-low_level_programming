ac8ef85aca17868267bf87f7cbcf8cfdd9c89998
paurbano <paurbano@gmail.com>
2019-10-01 21:14:04 +0000

    prints a string, in reverse, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_rev - entry point
+ * @s: string
+ * Description: prints a string, followed by a new line, to stdout.
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int i = 0, j;
+for (; *s != '\0' ; s++)
+i++;
+for (j = 0; j <= i ; j++)
+_putchar(*(s--));
+
+_putchar('\n');
+
+}
