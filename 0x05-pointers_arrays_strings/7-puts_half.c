71875e5e2c3948d651fdbfa6716cd04d290344a4
paurbano <paurbano@gmail.com>
2019-10-02 03:54:13 +0000

    function that prints half of a string, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * puts_half - entry point
+ * @str: string
+ * Description: prints a string, followed by a new line, to stdout.
+ * Return: void
+ */
+void puts_half(char *str)
+{
+int longitud = 0, indice;
+while (str[longitud] != '\0')
+longitud++;
+
+if (longitud % 2 == 0)
+indice = longitud / 2;
+else
+indice = (longitud - 1) / 2;
+
+for ( ; indice < longitud ; indice++)
+_putchar(str[indice]);
+
+_putchar('\n');
+}
