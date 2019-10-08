c3dfe543feddba900b884ffb8cd3bb836af58e20
paurbano <paurbano@gmail.com>
2019-10-08 05:01:41 +0000

    function that searches a string for any of a set of bytes.

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+/**
+ * _strpbrk - encodes a string using rot13.
+ * @s: array of char
+ * @accept: array
+ * Description:  function that encodes a string using rot13.
+ * Return: char string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int j;
+
+for (; *s != '\0' ; s++)
+{
+for (j = 0; accept[j] != '\0' ; j++)
+{
+if (*s == accept[j])
+{
+return (s);
+}
+}
+}
+return (NULL);
+}
