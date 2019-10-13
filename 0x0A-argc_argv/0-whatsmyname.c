d05eabad84afd66eae501a62864130db72b67688
paurbano <paurbano@gmail.com>
2019-10-13 21:56:49 +0000

    program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ * @argc : argc contains the number of arguments passed to the program
+ * @argv : A vector is a one-dimensional array
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+printf("%s\n", argv[0]);
+return (0);
+}
