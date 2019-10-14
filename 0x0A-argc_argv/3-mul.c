2041b97548eb9c0e60984f5cfe047047f63594c5
paurbano <paurbano@gmail.com>
2019-10-14 00:12:44 +0000

    program that multiplies two numbers.

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code for Holberton School students.
+ * @argc : argc contains the number of arguments passed to the program
+ * @argv : A vector is a one-dimensional array
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+int multiplica = 0;
+
+if (argc <= 2)
+{
+printf("Error \n");
+return (1);
+}
+else
+{
+multiplica = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", multiplica);
+}
+return (0);
+}
