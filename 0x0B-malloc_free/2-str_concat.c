acd9f07ff99c563fd179b165db7d909ca265bdfd
paurbano <55122159+paurbano@users.noreply.github.com>
2019-10-15 21:57:51 -0500

    Update 2-str_concat.c
    Fix function

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,23 +12,19 @@ char *str_concat(char *s1, char *s2)
 char *ptrStr;
 int  i, j, lenCad = 0;
 
-if (s1 ==  NULL && s1 ==  NULL)
-{
-ptrStr = "";
-return (ptrStr);
-}
 
-if (s1 !=  NULL)
-{
-for (i = 0; s1[i] != '\0' ; i++)
-lenCad++;
-}
 
-if (s2 !=  NULL)
-{
-for (i = 0; s2[i] != '\0' ; i++)
+if (s1 ==  NULL)
+s1 = "";
+
+if (s2 == NULL)
+s2 = "";
+
+for (i = 0, ; s1[i] != '\0' ; i++)
 lenCad++;
-}
+
+for (i = 0, ; s2[i] != '\0' ; i++)
+lenCad++;
 
 ptrStr =  malloc((lenCad + 1) * sizeof(char));
 
