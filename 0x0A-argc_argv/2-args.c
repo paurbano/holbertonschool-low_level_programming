320a4436997ef93a448c679fffab20c9b292d092
paurbano <paurbano@gmail.com>
2019-10-20 14:22:48 +0000

    prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- * main - check the code for Holberton School students.
+ * main - print all arguments receives
  * @argc : argc contains the number of arguments passed to the program
  * @argv : A vector is a one-dimensional array
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-  int multiplica = 0;
+  int i = 0;
 
-  if (argc <= 2)
-    {
-      printf("Error \n");
-      return (1);
-    }
-  else{
-    multiplica = atoi(argv[1]) * atoi(argv[2]);
-    printf("%s\n", multiplica);
-  }
+  for ( ; i < argc ; i++)
+  printf("%s\n", argv[i]);
+  
   return (0);
- }
+}
