a4638f340abe01d09000472ab4769823ad486e1a
paurbano <paurbano@gmail.com>
2019-10-21 23:22:54 +0000

    function that initialize a variable of type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+/**
+ * init_dog - function that initialize a variable of type struct dog
+ * @d: base
+ * @name: nombre
+ * @age: edad
+ * @owner: propietario
+ * Description: returns the natural square root of a number.
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+Struct d perro;
+
+perro->name = name;
+perro->age = age;
+perro->owner = owner;
+}
