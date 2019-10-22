9e8625539c1cb637f85bf6f72835d76bb411cf7a
paurbano <paurbano@gmail.com>
2019-10-22 02:07:39 +0000

    crea un perro

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,28 +5,21 @@
  * @name: struct
  * @age: edad
  * @owner: propietario
- * Description: function that creates a new dog.
  * Return: new_dog structure
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 struct dog *perro;
 int lonName = 0, lonOwner = 0, i = 0;
-
 if (name == NULL || owner == NULL)
 return (NULL);
-
 perro = malloc(sizeof(struct dog));
-
 if (perro == NULL)
 return (NULL);
-
 for ( ; name[lonName] != '\0' ; lonName++)
 ;
-
 for ( ; owner[lonOwner] != '\0' ; lonOwner++)
 ;
-
 perro->name = malloc(sizeof(char) * (lonName + 1));
 if (perro->name == NULL)
 {
@@ -38,9 +31,7 @@ return (NULL);
 for (i = 0 ; i < lonName ; i++)
 perro->name[i] = name[i];
 perro->name[i] = '\0';
-
 perro->age = age;
-
 perro->owner = malloc(sizeof(char) * (lonOwner + 1));
 if (perro->owner == NULL)
 {
@@ -48,10 +39,8 @@ free(perro->owner);
 free(perro);
 return (NULL);
 }
-
 for (i = 0 ; i < lonOwner ; i++)
 perro->owner[i] = owner[i];
 perro->owner[i] = '\0';
-
 return (perro);
 }
