7ec0db64fc7c1a7cd7e15aaaab2cc6be5740ede6
paurbano <paurbano@gmail.com>
2019-10-24 02:22:36 +0000

    calc

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - launch the app
+ * @argv: char
+ * @argc: int
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+int (*f)(int, int);
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+if ((strcmp(argv[2], "/") == 0 || (strcmp(argv[2], "%") == 0)) &&
+*argv[3] == '0')
+{
+printf("Error\n");
+exit(100);
+}
+if (strcmp(argv[2], "+") &&
+strcmp(argv[2], "-") &&
+strcmp(argv[2], "/") &&
+strcmp(argv[2], "*") &&
+strcmp(argv[2], "%"))
+{
+printf("Error\n");
+exit(99);
+}
+f = get_op_func(argv[2]);
+printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
+return (0);
+}
