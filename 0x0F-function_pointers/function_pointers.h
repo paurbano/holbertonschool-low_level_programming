c688ed30ac1a493e244341de2bd3ef62b6a81f3e
paurbano <55122159+paurbano@users.noreply.github.com>
2019-10-23 21:33:17 -0500

    Update function_pointers.h
    fixing

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,15 +1,9 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-#include <stdlib.h>
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 #include <stddef.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 #endif
