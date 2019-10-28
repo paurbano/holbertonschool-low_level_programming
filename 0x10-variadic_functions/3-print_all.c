e847f3878ad633409c06ea381fde4bd52161e5a3
paurbano <paurbano@gmail.com>
2019-10-28 01:01:04 +0000

    function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,21 +12,23 @@ int cont = 0;
 va_list parameters;
 
 va_start(parameters, format);
-
 while (*(format + cont) != '\0' && format != NULL)
 {
 switch (format[cont])
 {
 case 'c':
 printf("%c", va_arg(parameters, int));
+cont++;
 break;
 
 case 'i':
 printf("%d", va_arg(parameters, int));
+cont++;
 break;
 
 case 'f':
 printf("%f", va_arg(parameters, double));
+cont++;
 break;
 
 case 's':
@@ -34,13 +36,14 @@ cad = va_arg(parameters, char *);
 if (cad == NULL)
 cad = "(nil)";
 printf("%s", cad);
+cont++;
 break;
 
 default:
 cont++;
 continue;
 }
-++cont;
+cont++;
 if (*(format + cont) != '\0')
 printf(", ");
 }
