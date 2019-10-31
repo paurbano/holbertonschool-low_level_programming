0168515d2bc157aa5ed5caf3832aeaf349e3143a
paurbano <paurbano@gmail.com>
2019-10-31 18:38:37 +0000

    add string library

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define LISTS
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -23,5 +24,4 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
 #endif
