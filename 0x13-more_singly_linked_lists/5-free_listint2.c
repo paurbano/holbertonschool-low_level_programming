184f92ef32bea66090598df8588473a2189134d5
paurbano <paurbano@gmail.com>
2019-11-05 07:51:06 +0000

    fix double pointer reference to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,6 +8,7 @@
 void free_listint2(listint_t **head)
 {
 listint_t *temp;
+
 if (head == NULL)
 return;
 
@@ -17,6 +18,6 @@ temp = (*head)->next;
 free(*head);
 *head = temp;
 }
-free(*head);
-head = NULL;
+
+*head = NULL;
 }
