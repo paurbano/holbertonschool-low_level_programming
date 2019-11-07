12684b3518f47f8347c7e53924205ae6a0ecacc5
paurbano <paurbano@gmail.com>
2019-11-07 23:03:44 +0000

    header file

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,5 +1,8 @@
 #ifndef BITWISE
 #define BITWISE
+#include <stdlib.h>
+#include <stdio.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
