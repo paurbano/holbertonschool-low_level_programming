4925b5f38df0ea73a8fe9a0bf4e51a2122b79910
paurbano <paurbano@gmail.com>
2019-11-15 00:24:56 +0000

    copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,21 +12,21 @@ int fileFrom, fileTo, rd, wr;
 char buf[1024];
 if (argc != 3)
 {
-dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 
 fileFrom = open(argv[1], O_RDONLY);
 if (fileFrom == -1)
 {
-dprintf(STDOUT_FILENO, "Error: Can't read from %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
 
 fileTo = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (fileTo == -1)
 {
-dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
 
@@ -35,25 +35,25 @@ for (rd = read(fileFrom, buf, 1024); rd > 0; rd = read(fileFrom, buf, 1024))
 wr = write(fileTo, buf, rd);
 if (wr == -1)
 {
-dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
 exit(99);
 }
 }
 
 if (rd == -1)
 {
-dprintf(STDOUT_FILENO, "Error: Can't read from %s\n", argv[1]);
-exit(99);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
 }
 
 if (close(fileFrom) == -1)
 {
-dprintf(STDOUT_FILENO, "Error: Can't close fd %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[1]);
 exit(100);
 }
 if (close(fileTo) == -1)
 {
-dprintf(STDOUT_FILENO, "Error: Can't close fd %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[2]);
 exit(100);
 }
 return (0);
