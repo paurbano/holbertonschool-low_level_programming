10982e1e12c85b560cd5cb398299f326ca2fc46b
paurbano <paurbano@gmail.com>
2019-12-15 23:41:07 +0000

    add validation

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,21 +14,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
+	if (*h == NULL && idx != 0)
+		return (NULL)
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
 	new->prev = NULL;
-	if (*h == NULL)
-	{
-		(*h) = new;
-		return (new);
-	}
-	if (idx == 0)
-	{
-		return (add_dnodeint(h, n));
-	}
+
 	while (actual != NULL)
 	{
 		if (idx == cont && actual->next != NULL)
@@ -41,8 +38,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 		if (actual->next == NULL)
 		{
-			new->prev = actual;
 			actual->next = new;
+			new->prev = actual;
 			return (new);
 		}
 		cont++;
