4125c569fe0c34dcad2c949a7510e1f7773fe9a7
paurbano <paurbano@gmail.com>
2019-12-16 03:12:57 +0000

    change assignations inside while

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,8 +32,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			new->next = actual->next;
 			actual->prev->next = new;
-			new->prev = actual;
-			actual->next = new;
+			/*new->prev = actual;*/
+			new->prev = actual->prev;
+			/*actual->next = new;*/
+			actual->prev = new;
 			return (new);
 		}
 		if (actual->next == NULL && cont == idx)
